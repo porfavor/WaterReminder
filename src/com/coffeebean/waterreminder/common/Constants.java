97d07e6b97321eca79c870d6c4c7c581eae41ba0
porfavor <chris.hu.cohen@gmail.com>
2013-07-11 11:31:14 +0800

    move all constants to this file

---
+++ src/com/coffeebean/waterreminder/common/Constants.java
@@ -0,0 +1,6 @@
+package com.coffeebean.waterreminder.common;
+
+public class Constants {
+	public static final String DATABASE_NAME = "water_reminder.db";  
+    public static final int DATABASE_VERSION = 1;
+}
