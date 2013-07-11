f26b7be410a658a8fe345415a746acc3a50419a1
porfavor <chris.hu.cohen@gmail.com>
2013-07-11 11:38:42 +0800

    add DATA_TABLE

--- src/com/coffeebean/waterreminder/common/Constants.java
+++ src/com/coffeebean/waterreminder/common/Constants.java
@@ -1,6 +1,8 @@
 package com.coffeebean.waterreminder.common;
 
 public class Constants {
-	public static final String DATABASE_NAME = "water_reminder.db";  
+	  
     public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "water_reminder.db";
+    public static final String DATA_TABLE = "remind_time";
 }
