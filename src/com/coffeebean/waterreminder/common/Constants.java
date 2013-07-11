7521065b96f2d430e44e9cc2396f300cf350222f
porfavor <chris.hu.cohen@gmail.com>
2013-07-11 15:56:48 +0800

    Add constants

--- src/com/coffeebean/waterreminder/common/Constants.java
+++ src/com/coffeebean/waterreminder/common/Constants.java
@@ -5,4 +5,11 @@ public class Constants {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "water_reminder.db";
     public static final String DATA_TABLE = "remind_time";
+    
+    public static final int NUMBER = 8;
+    public static final int DEFAULT_HOUR = 7;
+    public static final int DEFAULT_MINUTE = 0;
+    public static final int DEFAULT_SECOND = 0;
+    
+    public static final String[] PREFIX = {"第一杯水：", "第二杯水：", "第三杯水：", "第四杯水：", "第五杯水：", "第六杯水：", "第七杯水：", "第八杯水："};
 }
