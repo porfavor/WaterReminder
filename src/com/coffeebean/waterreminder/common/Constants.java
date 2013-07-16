e390c84e62ca0ae489f1602463358b7a0238633e
porfavor <chris.hu.cohen@gmail.com>
2013-07-16 22:01:58 +0800

    add constant

--- src/com/coffeebean/waterreminder/common/Constants.java
+++ src/com/coffeebean/waterreminder/common/Constants.java
@@ -1,19 +1,21 @@
 package com.coffeebean.waterreminder.common;
 
 public class Constants {
-	  
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "water_reminder.db";
-    public static final String DATA_TABLE = "remind_time";
-    
-    public static final int NUMBER = 8;
-    public static final int DEFAULT_HOUR = 7;
-    public static final int DEFAULT_MINUTE = 0;
-    public static final int DEFAULT_SECOND = 0;
-    
-    public static final String[] PREFIX = {"第一杯水：", "第二杯水：", "第三杯水：", "第四杯水：", "第五杯水：", "第六杯水：", "第七杯水：", "第八杯水："};
 
-    public static final int MSG_SOUND_LOAD_OK = 1;
-    
-    public static final int MSG_UI_UPDATE_TIME = 1;
+	public static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "water_reminder.db";
+	public static final String DATA_TABLE = "remind_time";
+
+	public static final int NUMBER = 8;
+	public static final int DEFAULT_HOUR = 7;
+	public static final int DEFAULT_MINUTE = 0;
+	public static final int DEFAULT_SECOND = 0;
+	public static final long DAY_IN_MILLISEC = 24 * 60 * 60 * 1000;
+
+	public static final String[] PREFIX = { "第一杯水：", "第二杯水：", "第三杯水：", "第四杯水：",
+			"第五杯水：", "第六杯水：", "第七杯水：", "第八杯水：" };
+
+	public static final int MSG_SOUND_LOAD_OK = 1;
+
+	public static final int MSG_UI_UPDATE_TIME = 1;
 }
